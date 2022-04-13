93ff3884a463ea1a420b17d5373f0099be154f58
Ajay007 <ajaybendre@credenceanalytics.com>
2022-04-14 00:47:03 +0530

    added firebase file

---
+++ src/utils/firebase.js
@@ -0,0 +1,16 @@
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA_5Th9an9YoGndJgJv57WE0Nz4mZBfj94",
+    authDomain: "creditbuddy-official.firebaseapp.com",
+    projectId: "creditbuddy-official",
+    storageBucket: "creditbuddy-official.appspot.com",
+    messagingSenderId: "681921788389",
+    appId: "1:681921788389:web:3751cdedb8126ed0c3570c"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  export default app;
