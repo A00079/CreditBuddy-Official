4a231032bbc9b2e38d062b026fce433603c55eef
Ajay007 <ajaybendre@credenceanalytics.com>
2022-04-15 12:54:48 +0530

    Fixes for gendre alignment

--- src/pages/CreditCard/Components/CreditForm/CreditForm.js
+++ src/pages/CreditCard/Components/CreditForm/CreditForm.js
@@ -40,7 +40,7 @@ const Creditform = (props) => {
             "averageAnnualIncome": input.averageannualincome,
             "gender": input.gender
         }
-        console.log('Data ===>',data);
+        console.log('Data ===>', data);
         // axios.post('https://credit.candidleads.com/api/v1/details/adddetail?type=creditcard', data)
         //     .then(function (response) {
         //         console.log(response);
@@ -100,10 +100,10 @@ const Creditform = (props) => {
                                 <input onChange={handleInputChange} type="number" id="averageannualincome" name="averageannualincome" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                             </div>
                         </div>
-                        <div className='w-full flex flex-col sm:flex-row justify-between sm:space-x-8 mb-4'>
+                        <div className='sm:w-1/2 w-full flex flex-col sm:flex-row justify-between sm:space-x-8 mb-4 sm:pr-4'>
                             <div className="relative w-full inline-block text-left">
                                 <div>
-                                    <button onClick={() => toggleOption()} type="button" className="inline-flex justify-between w-full rounded-md border border-gray-300 shadow-sm px-3 py-3 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" id="menu-button" aria-expanded="true" aria-haspopup="true">
+                                    <button onClick={() => toggleOption('Gender')} type="button" className="inline-flex justify-between w-full rounded-md border border-gray-300 shadow-sm px-3 py-3 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" id="menu-button" aria-expanded="true" aria-haspopup="true">
                                         {selectedOption}
                                         <svg className="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                             <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
@@ -111,7 +111,7 @@ const Creditform = (props) => {
                                     </button>
                                 </div>
                                 <div className={isOpenDrop ? 'block' : 'hidden'}>
-                                    <div className="origin-top-right absolute right-0 mt-2 w-64 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1">
+                                    <div className="origin-top-right absolute right-0 mt-2 w-full rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1">
                                         <div className="py-1" role="none">
                                             <p onClick={() => toggleOption('Male')} className="text-gray-700 hover:bg-gray-100 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-0">Male</p>
                                             <p onClick={() => toggleOption('Female')} className="text-gray-700 hover:bg-gray-100 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-1">Female</p>
